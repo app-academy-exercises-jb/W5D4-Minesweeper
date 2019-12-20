4ae866081c7cb607a14ce4ca2f9b839b4e03c078
Jorge Barreto <jrbd93@gmail.com>
2019-12-20 18:19:54 -0500

    save game functionality added

--- game.rb
+++ game.rb
@@ -3,14 +3,26 @@ require_relative 'board.rb'
 
 class Minesweeper
   attr_reader :board
-  def initialize(n)
-    @board = Board.new(n)
+  def self.from_file(filename)
+    raise "not a proper file" unless File.file?(filename)
+    file = File.open(filename, "rb")
+    board = Marshal.load(file)
+    file.close
+    self.new(0, board)
+  end
+
+  def initialize(n, board=nil)
+    if board.nil?
+      @board = Board.new(n) 
+    else
+      @board = board
+    end
     @over = false
 
     play until over? 
     system('clear')
     @board.render
-    puts "you won!" if @win
+    @win ? (puts "you won!") : (puts "you lost!")
   end
 
   def play
@@ -29,16 +41,24 @@ class Minesweeper
       @over = true unless @board.reveal(@last_move[1])
     when "f"
       @board.flag(@last_move[1])
+    when "S"
+      file = File.new("game", "w+b")
+      file.write(Marshal.dump(@board))
+      file.close
+
+      puts "game saved!"
+      @over = true
     else
       raise "fatal"
     end
   end
 
   def valid_input?(str)
-    return false unless str.length == 4
+    return false unless str.length == 4 || str.length == 1
     command = str.chars.shift
-    return false unless command == "r" or command == "f"
-    location = str[1..-1].split(",").map(&:to_i)
+    return false unless command == "r" || command == "f" || command == "S"
+    loc_parse = str[1..-1]
+    location = loc_parse.length == 0 ? [] : str[1..-1].split(",").map(&:to_i)
     return false unless location.all? { |ele| ele.is_a?(Integer) }
     @last_move = [command, location]
     true
